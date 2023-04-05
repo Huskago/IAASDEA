f98d02fc18076f991dfe87d6a42b840cb59e3247
Huskago <tiagoflfl0202@gmail.com>
2023-04-06 01:28:29 +0200

    optimize and comment

--- predict.py
+++ predict.py
@@ -18,33 +18,60 @@ class_names = ['Bad', 'Good']
 # Configurer la caméra
 cap = cv2.VideoCapture(0)
 
+# Réduire la résolution de l'image
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+# Boucle principale
 while True:
+    # Capturer une image
     ret, frame = cap.read()
     if not ret:
         print("Unable to capture video")
         break
         
+    # Réduire la taille de l'image
+    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    
+    # Convertir l'image en niveaux de gris
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Détecter les visages dans l'image
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
+    # Classer chaque visage détecté
     for (x, y, w, h) in faces:
+        # Extraire l'image du visage
         face_img = gray[y:y+w, x:x+w]
-        resized = cv2.resize(face_img, (48, 48), interpolation=cv2.INTER_AREA)
+        
+        # Redimensionner l'image pour l'entrée du modèle
+        resized = cv2.resize(face_img, input_shape[:2], interpolation=cv2.INTER_AREA)
+    
+        # Prétraiter l'image pour la passer en entrée du modèle
         img_tensor = keras.preprocessing.image.img_to_array(resized)
         img_tensor = np.expand_dims(img_tensor, axis=0)
         img_tensor /= 255.
         
+        # Prédire la classe de l'image du visage
         prediction = model.predict(img_tensor)
+        
+        # Afficher la classe prédite sur la frame
         if prediction[0][0] > prediction[0][1]:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            cv2.putText(frame, "Bad", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            # Afficher un rectangle rouge autour du visage si la classe prédite est 'Bad'
+            cv2.rectangle(frame, (x * 2, y * 2), ((x + w) * 2, (y + h) * 2), (0, 0, 255), 2)
+            cv2.putText(frame, "Bad", (x * 2, y * 2 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
         else:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(frame, "Good", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            # Afficher un rectangle vert autour du visage si la classe prédite est 'Good'
+            cv2.rectangle(frame, (x * 2, y * 2), ((x + w) * 2, (y + h) * 2), (0, 255, 0), 2)
+            cv2.putText(frame, "Good", (x * 2, y * 2 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
+    # Afficher la frame
     cv2.imshow('Face Classification', frame)
+    
+    # Quitter le programme si la touche 'q' est pressée
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Libérer la caméra et fermer toutes les fenêtres
 cap.release()
 cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -6,14 +6,14 @@ train_dir = "dataset/train"
 val_dir = "dataset/validation"
 
 # Définir les paramètres d'entrainement du modèle
-batch_size = 32
-epochs = 100
-input_shape = (48, 48, 1)
-num_classes = 2
+batch_size = 32 # Nombre d'images utilitées pour une itération du gradient descendant
+epochs = 100 # Nombre d'itérations sur l'ensemble des données d'entrainement
+input_shape = (48, 48, 1) # Dimensions des images d'entrée (largeur, hauteur, nombre de canaux)
+num_classes = 2 # Nomre de classes de sortie (2 pour un problème de classification binaire)
 
 # Prétraiter les images en les redimensionnant et en les normalisant
-train_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = train_datagen.flow_from_directory(train_dir, target_size=input_shape[:2], color_mode='grayscale', batch_size=batch_size, class_mode='categorical', shuffle=True)
+train_datagen = ImageDataGenerator(rescale=1./255) # Applique une mise à l'échelle à chaque pixel pour normaliser les valeurs de chaque canal de couleur entre 0 et 1
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=input_shape[:2], color_mode='grayscale', batch_size=batch_size, class_mode='categorical', shuffle=True) # Génère des lots d'images de taille batch_size, redimensionnées à target_size et converties en niveau de gris, avec des étiquettes catégorielles pour chaque classe
 
 val_datagen = ImageDataGenerator(rescale=1./255)
 val_generator = val_datagen.flow_from_directory(val_dir, target_size=input_shape[:2], color_mode='grayscale', batch_size=batch_size, class_mode='categorical', shuffle=True)
@@ -31,10 +31,10 @@ model = keras.Sequential([
 ])
 
 # Compiler le modèle
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # Définit l'optimiseur, la fonction de perte et les métriques à utiliser pendant l'entraînement
 
 # Entrainement du modèle
-history = model.fit(train_generator, epochs=epochs, validation_data=val_generator)
+history = model.fit(train_generator, epochs=epochs, validation_data=val_generator)  # Entraîne le modèle avec les données d'entraînement générées par le générateur train_generator, et valide avec les données générées par val_generator
 
 # Sauvegarder le modèle
-model.save('face_classification_model.h5')
+model.save('face_classification_model.h5') # Enregistre le modèle au format H5 pour une utilisation ultérieure
